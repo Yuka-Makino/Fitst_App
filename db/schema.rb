5489d158fa0d081908068dfd335e60d0ff427ad8
Yuka-Makino <ec2-user@ip-172-31-28-11.ap-northeast-1.compute.internal>
2023-06-17 13:33:24 +0000

    Rails RESST

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_150000) do
+ActiveRecord::Schema.define(version: 2023_06_17_132243) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
